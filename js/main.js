4a9234a7657cd1c4b9c676fe1ad805577f4dc087
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-01-06 17:14:24 -0300

    Add Absolute Path

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ win.setShowInTaskbar(false);
 win.resizeTo(Math.round(screen.width), Math.round(screen.height));
 win.setResizable(false);
 
-exec('python', ["/usr/share/Lynx/lynx-dock/scripts/setDesktop.py"]);
+exec('python', ["/usr/share/Lynx/lynx-desktop/scripts/setDesktop.py"]);
 
 const video = document.getElementById('video-background');
 video.src = `file://${homePath}.config/lynx/background/tropical-sunset.mp4`;
