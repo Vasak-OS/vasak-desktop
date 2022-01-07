9fb18beabf5ca7d8a107ae43d6de13420e3fd77d
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-01-07 17:35:12 -0300

    Set Overflow auto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Set windows Properties
 win.setShowInTaskbar(false);
-win.resizeTo(Math.round(screen.width), Math.round(screen.height));
+win.resizeTo(Math.round(screen.width)-1, Math.round(screen.height)-1);
 win.setResizable(false);
 
 exec('python', ["/usr/share/Lynx/lynx-desktop-service/Setters/setDesktop.py"]);
