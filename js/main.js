5febf0e43804a793e5ea6285c986176ecacd8d5e
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-05-17 13:15:09 -0300

    Set position

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Set windows Properties
 win.setShowInTaskbar(false);
 win.setResizable(false);
+win.y = 0;
+win.x = 0;
 win.setVisibleOnAllWorkspaces(true);
 
 exec('python', ['/usr/share/Lynx/lynx-desktop-service/Setters/setDesktop.py', `${process.pid.toString()}`]);
