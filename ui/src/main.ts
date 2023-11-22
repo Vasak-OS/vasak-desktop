aa754d57fa35cec01ba84f2ab3a4d0af30a9c0a9
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-11-22 08:41:36 -0300

    Update main.ts file to use Pinia for state management

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const startApp = (channel: any) => {
+  const app = createApp(App);
+  app.config.globalProperties.$vsk = channel.objects.vsk;
+  app.use(createPinia());
+
+  app.mount("#app");
+};
+
+// @ts-ignore
+new QWebChannel(qt.webChannelTransport, startApp);
