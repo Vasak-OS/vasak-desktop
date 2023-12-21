f7db13cbffcc8cf5e7110a6e513107248438f529
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-21 09:37:36 -0300

    Add set_as_desktop method to DesktopWindow class

--- src/DesktopWindow.py
+++ src/DesktopWindow.py
@@ -1,5 +1,6 @@
 from Vasak.VSKWindow import VSKWindow
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt
 from src.DesktopBinding import DesktopBinding
 
 class DesktopWindow(VSKWindow):
@@ -11,7 +12,13 @@ class DesktopWindow(VSKWindow):
         self.set_as_desktop() # Hacer que la ventana se comporte como un dock
         self.load_html("ui/dist/index.html") # Cargar un HTML en el WebView
         
-    
     # Mover la ventana a una pantalla específica
     def move_to_screen(self):
         self.setGeometry(0, 0, QApplication.primaryScreen().availableGeometry().width(), QApplication.primaryScreen().availableGeometry().height())
+    
+    def set_as_desktop(self):
+        self.setAttribute(Qt.WidgetAttribute.WA_X11NetWmWindowTypeDesktop, True)  # Seteo tipo desktop x11
+        self.setAttribute(Qt.WidgetAttribute.WA_AlwaysStackOnTop, False)
+        self.setWindowFlags(
+            self.windowFlags() | Qt.WindowType.FramelessWindowHint | Qt.WindowType.Desktop
+        )
