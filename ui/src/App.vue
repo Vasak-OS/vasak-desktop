92c16ed130bda897e8aa60cd145ed873e3bffed5
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-10-23 10:34:54 -0300

    Refactorizar componente App.vue para mejorar rendimiento y seguir convenciones de Vue 3

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,50 +1,41 @@
-<script lang="ts">
+<script lang="ts" setup>
+import { onMounted, Ref, ref } from "vue";
 import ClockWidget from "./components/widgets/ClockWidgert.vue";
 //import background from "./assets/background.mp4";
 
-const background = '/home/pato/.backgroud.mp4';
-export default {
-	name: "App",
-	components: {
-		ClockWidget,
-	},
-	data() {
-		return {
-			background: '',
-		};
-	},
-	methods: {
-		async setBackground() : Promise<void>  {
-			this.background = await (this as any).$vsk.getHome() + '/.config/vsk/background.mp4';
-		},
-	},
-	mounted() {
-		this.setBackground();	
-	},
+const background: Ref<string> = ref("/home/pato/.backgroud.mp4");
+
+const setBackground = async (): Promise<void> => {
+  background.value =
+    (await (this as any).$vsk.getHome()) + "/.config/vsk/background.mp4";
 };
+
+onMounted(() => {
+  setBackground();
+});
 </script>
 
 <template>
   <div class="global">
-		<video
-			style="border-radius:0px;"
-			id="video-background"
-			type="video/mp4"
-			:src="background"
-			loop
-			autoplay
-			muted>
-		</video>
-		<ClockWidget />
-	</div>
+    <video
+      style="border-radius: 0px"
+      id="video-background"
+      type="video/mp4"
+      :src="background"
+      loop
+      autoplay
+      muted
+    ></video>
+    <ClockWidget />
+  </div>
 </template>
 
 <style>
 body,
 #app,
 .global {
-	overflow: auto;
-	width: 100vw;
-	height: 100vh;
+  overflow: auto;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
