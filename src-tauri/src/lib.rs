348fbd059fe074cffaa22bf00f0c8f1a072aa147
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-05-08 14:56:28 -0300

    refactor: remove unused icon handling and SVG files to clean up the project

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,8 @@
-#[path = "./commands/icons.rs"]
-mod icons;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            icons::get_icon_base64,
-            icons::get_symbol_base64
-        ])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
