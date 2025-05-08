e7b7c2cb19ce649ec2db2e63eb67e239014b2d2c
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-05-08 16:06:38 -0300

    feat: enhance window management by adding focus handling and property setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,29 @@
+use tauri::Manager;
+use gtk::traits::GtkWindowExt;
+
+fn set_window_properties(window: &tauri::WebviewWindow) {
+    let gtk_window = window.gtk_window().expect("Failed to get GTK window");
+
+    gtk_window.set_type_hint(gdk::WindowTypeHint::Desktop);
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+            }
+        }))
         .invoke_handler(tauri::generate_handler![])
+        .setup(move |app| {
+            let window = app
+                .get_webview_window("main")
+                .expect("main window not found");
+            set_window_properties(&window);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
