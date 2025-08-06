e1071d879605dd1872f5f3f4d86ce04d736690d9
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-06 12:38:34 -0300

    feat: implement vue-router for enhanced navigation

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { createMemoryHistory, createRouter } from "vue-router";
+
+import DesktopView from "../views/DesktopView.vue";
+import PanelView from "../views/PanelView.vue";
+
+const routes = [
+  { path: "/desktop", component: DesktopView },
+  { path: "/panel", component: PanelView },
+];
+
+export const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
