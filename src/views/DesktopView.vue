b0a2c0fb39eaa6dda47fa111bbbaf7c29cf6ac60
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-06 12:38:46 -0300

    feat: refactor App.vue to use RouterView and create DesktopView.vue for improved layout

---
+++ src/views/DesktopView.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import ClockWidget from "@/components/widgets/ClockWidget.vue";
+import { ref } from "vue";
+
+const background = ref(
+  "https://png.pngtree.com/background/20250102/original/pngtree-planetary-glow-stunning-celestial-view-pc-wallpaper-picture-image_15736228.jpg"
+);
+const backgroundType = ref("image/jpeg");
+</script>
+
+<template>
+  <video
+    v-if="backgroundType.includes('video')"
+    style="border-radius: 0px"
+    :type="backgroundType"
+    :src="background"
+    class="w-screen h-screen object-cover absolute z-10"
+    loop
+    autoplay
+    muted
+  ></video>
+  <img
+    v-else
+    :src="background"
+    class="w-screen h-screen object-cover absolute z-10"
+    style="border-radius: 0px"
+  />
+  <main
+    class="w-screen h-screen flex items-center justify-center absolute z-20"
+  >
+    <ClockWidget />
+  </main>
+</template>
