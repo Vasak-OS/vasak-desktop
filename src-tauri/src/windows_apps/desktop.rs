a21ce55c70511dd949782eb56e449a6a84f7c892
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-07 11:40:40 -0300

    feat: agregar configuración de ventana para el escritorio y ajustar la creación de paneles

--- src-tauri/src/windows_apps/desktop.rs
+++ src-tauri/src/windows_apps/desktop.rs
@@ -1,31 +1,59 @@
 use gtk::prelude::*;
-use tauri::{AppHandle, WebviewUrl, WebviewWindowBuilder};
+use tauri::{App, Manager};
 
-use crate::monitor_manager::get_monitors;
+use crate::monitor_manager::{get_monitors, get_primary_monitor};
 
-pub fn create_desktops(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
-    let monitors = get_monitors(app).ok_or("No monitors found")?;
+pub fn create_desktops(app: &App) -> Result<(), Box<dyn std::error::Error>> {
+    let _monitors = get_monitors(app.handle()).ok_or("No monitors found")?;
+    let primary_monitor = get_primary_monitor(app.handle()).ok_or("No primary monitor found")?;
 
-    for (index, monitor) in monitors.iter().enumerate() {
-        let monitor_size = monitor.size();
-        let monitor_position = monitor.position();
+    let primary_monitor_size = primary_monitor.size();
+    let primary_monitor_position = primary_monitor.position();
 
-        let desktop_window = WebviewWindowBuilder::new(
-            app,
-            &format!("desktop_{}", index),
-            WebviewUrl::App(format!("index.html#/desktop?monitor={}", index).into()),
-        )
-        .title(&format!("Vasak Desktop {}", index))
-        .decorations(false)
-        .position(monitor_position.x as f64, monitor_position.y as f64)
-        .inner_size(monitor_size.width as f64, monitor_size.height as f64)
-        .max_inner_size(monitor_size.width as f64, monitor_size.height as f64)
-        .min_inner_size(monitor_size.width as f64, monitor_size.height as f64)
-        .skip_taskbar(true)
-        .build()?;
+    let desktop_window = app
+        .get_webview_window("desktop")
+        .expect("desktop window not found");
 
-        set_window_properties(&desktop_window);
-    }
+    desktop_window.set_position(tauri::Position::Physical(tauri::PhysicalPosition {
+        x: primary_monitor_position.x,
+        y: primary_monitor_position.y,
+    }))?;
+
+    desktop_window.set_size(tauri::Size::Physical(tauri::PhysicalSize {
+        width: primary_monitor_size.width,
+        height: primary_monitor_size.height,
+    }))?;
+
+    desktop_window.set_max_size(Some(tauri::Size::Physical(tauri::PhysicalSize {
+        width: primary_monitor_size.width,
+        height: primary_monitor_size.height,
+    })))?;
+
+    desktop_window.set_min_size(Some(tauri::Size::Physical(tauri::PhysicalSize {
+        width: primary_monitor_size.width,
+        height: primary_monitor_size.height,
+    })))?;
+
+    // for (index, monitor) in monitors.iter().enumerate() {
+    //     let monitor_size = monitor.size();
+    //     let monitor_position = monitor.position();
+
+    //     let desktop_window = WebviewWindowBuilder::new(
+    //         app,
+    //         &format!("desktop_{}", index),
+    //         WebviewUrl::App(format!("index.html#/desktop?monitor={}", index).into()),
+    //     )
+    //     .title(&format!("Vasak Desktop {}", index))
+    //     .decorations(false)
+    //     .position(monitor_position.x as f64, monitor_position.y as f64)
+    //     .inner_size(monitor_size.width as f64, monitor_size.height as f64)
+    //     .max_inner_size(monitor_size.width as f64, monitor_size.height as f64)
+    //     .min_inner_size(monitor_size.width as f64, monitor_size.height as f64)
+    //     .skip_taskbar(true)
+    //     .build()?;
+
+    //     set_window_properties(&desktop_window);
+    // }
 
     Ok(())
 }
