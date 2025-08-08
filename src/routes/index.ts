280f82c765b76a5b338d8252f0e69d574636d5a8
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-08 09:19:35 -0300

    feat: agregar vista de menú y actualizar rutas para incluirla

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 
 import DesktopView from "../views/DesktopView.vue";
+import MenuView from "../views/MenuView.vue";
 import PanelView from "../views/PanelView.vue";
 
 const routes = [
   { path: "/desktop", component: DesktopView },
   { path: "/panel", component: PanelView },
+  { path: "/menu", component: MenuView },
 ];
 
 export const router = createRouter({
