55b4322d8e849e5bc9a602661edb6ceaba826860
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-11 17:31:51 -0300

    feat: agregar componentes de menú y clima, incluyendo SearchMenuComponent, WeatherWidget y otros elementos de interfaz

---
+++ src/components/cards/AppMenuCard.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { ref, onMounted, Ref } from "vue";
+import { getIconSource } from "@vasakgroup/plugin-vicons";
+import { invoke } from "@tauri-apps/api/core";
+
+const props = defineProps({
+  app: {
+    type: Object,
+    required: true,
+  },
+});
+
+const appIcon: Ref<string> = ref(props.app.icon);
+
+const openApp = async (path: string) => {
+  try {
+    await invoke("open_app", { path });
+  } catch (error) {
+    console.error("Error al abrir la aplicación:", error);
+  }
+};
+
+const getAppIcon = async () => {
+  appIcon.value = await getIconSource(props.app.icon);
+};
+
+onMounted(() => {
+  getAppIcon();
+});
+</script>
+
+<template>
+  <button
+    class="flex flex-row w-full p-2 rounded-vsk items-center transform hover:translate-x-1 hover:scale-110 hover:bg-vsk-primary/50 transition-transform"
+    @click="openApp(app.path)"
+    :title="app.description"
+  >
+    <img :src="appIcon" :alt="app.name" :title="app.name" class="img-fluid h-10" />
+    <div class="col-10 app-card-info ps-2">
+      {{ app.name }}
+      <span class="text-muted" style="display: none">{{
+        app.description
+      }}</span>
+      <span style="display: none">{{ app.keywords }}</span>
+    </div>
+  </button>
+</template>
---
+++ src/components/cards/UserMenuCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, onMounted, computed, Ref } from "vue";
+import { getUserData, type UserInfo } from "@vasakgroup/plugin-user-data";
+
+const userInfo: Ref<UserInfo | null> = ref<UserInfo | null>(null);
+
+const avatarSrc = computed(() => {
+  return userInfo.value?.avatar_data;
+});
+
+const loadUserInfo = async () => {
+  try {
+    userInfo.value = await getUserData();
+  } catch (error) {
+    console.error("Error al cargar información del usuario:", error);
+  }
+};
+
+onMounted(loadUserInfo);
+</script>
+
+<template>
+  <div v-if="userInfo" class="flex items-center space-x-3 backdrop-blur-sm">
+    <img
+      :src="avatarSrc"
+      :alt="userInfo.username"
+      class="w-10 h-10 rounded-full object-cover"
+    />
+    <div class="flex flex-col">
+      <span class="text-sm font-bold">{{ userInfo.full_name }}</span>
+      <span class="text-xs text-white/70">@{{ userInfo.username }}</span>
+    </div>
+  </div>
+</template>
