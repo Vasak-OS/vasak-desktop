c09af81b555fc79f272bd5bc2e54ff1a198394a5
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-11 18:41:06 -0300

    feat: agregar comandos para gestionar el menú y la sesión, incluyendo apertura, cierre, y detección del servidor de visualización

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,3 +1,9 @@
+mod menu;
+mod runner;
+mod session;
 mod window_manager;
 
+pub use menu::{get_menu_items, toggle_menu};
+pub use runner::open_app;
+pub use session::{detect_display_server, logout, shutdown, reboot, suspend};
 pub use window_manager::{get_windows, toggle_window};
