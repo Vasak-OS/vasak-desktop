a17955c4844524d22b06af29bb708aa702b880b2
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-25 16:29:08 -0300

    feat: refactor WeatherIcon component to use setup syntax and improve icon handling

--- src/components/icon/WeatherIcon.vue
+++ src/components/icon/WeatherIcon.vue
@@ -1,322 +1,328 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { onMounted, Ref, ref } from "vue";
+import { getIconSource } from "@vasakgroup/plugin-vicons";
 
-const codeData = {
-  '0': {
-    day: {
-      description: 'Sunny',
-      image: 'http://openweathermap.org/img/wn/01d@2x.png'
-    },
-    night: {
-      description: 'Clear',
-      image: 'http://openweathermap.org/img/wn/01n@2x.png'
-    }
-  },
-  '1': {
-    day: {
-      description: 'Mainly Sunny',
-      image: 'http://openweathermap.org/img/wn/01d@2x.png'
-    },
-    night: {
-      description: 'Mainly Clear',
-      image: 'http://openweathermap.org/img/wn/01n@2x.png'
-    }
-  },
-  '2': {
-    day: {
-      description: 'Partly Cloudy',
-      image: 'http://openweathermap.org/img/wn/02d@2x.png'
-    },
-    night: {
-      description: 'Partly Cloudy',
-      image: 'http://openweathermap.org/img/wn/02n@2x.png'
-    }
-  },
-  '3': {
-    day: {
-      description: 'Cloudy',
-      image: 'http://openweathermap.org/img/wn/03d@2x.png'
-    },
-    night: {
-      description: 'Cloudy',
-      image: 'http://openweathermap.org/img/wn/03n@2x.png'
-    }
-  },
-  '45': {
-    day: {
-      description: 'Foggy',
-      image: 'http://openweathermap.org/img/wn/50d@2x.png'
-    },
-    night: {
-      description: 'Foggy',
-      image: 'http://openweathermap.org/img/wn/50n@2x.png'
-    }
-  },
-  '48': {
-    day: {
-      description: 'Rime Fog',
-      image: 'http://openweathermap.org/img/wn/50d@2x.png'
-    },
-    night: {
-      description: 'Rime Fog',
-      image: 'http://openweathermap.org/img/wn/50n@2x.png'
-    }
-  },
-  '51': {
-    day: {
-      description: 'Light Drizzle',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Light Drizzle',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '53': {
-    day: {
-      description: 'Drizzle',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Drizzle',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '55': {
-    day: {
-      description: 'Heavy Drizzle',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Heavy Drizzle',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '56': {
-    day: {
-      description: 'Light Freezing Drizzle',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Light Freezing Drizzle',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '57': {
-    day: {
-      description: 'Freezing Drizzle',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Freezing Drizzle',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '61': {
-    day: {
-      description: 'Light Rain',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png'
-    },
-    night: {
-      description: 'Light Rain',
-      image: 'http://openweathermap.org/img/wn/10n@2x.png'
-    }
-  },
-  '63': {
-    day: {
-      description: 'Rain',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png'
-    },
-    night: {
-      description: 'Rain',
-      image: 'http://openweathermap.org/img/wn/10n@2x.png'
-    }
-  },
-  '65': {
-    day: {
-      description: 'Heavy Rain',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png'
-    },
-    night: {
-      description: 'Heavy Rain',
-      image: 'http://openweathermap.org/img/wn/10n@2x.png'
-    }
-  },
-  '66': {
-    day: {
-      description: 'Light Freezing Rain',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png'
-    },
-    night: {
-      description: 'Light Freezing Rain',
-      image: 'http://openweathermap.org/img/wn/10n@2x.png'
-    }
-  },
-  '67': {
-    day: {
-      description: 'Freezing Rain',
-      image: 'http://openweathermap.org/img/wn/10d@2x.png'
-    },
-    night: {
-      description: 'Freezing Rain',
-      image: 'http://openweathermap.org/img/wn/10n@2x.png'
-    }
-  },
-  '71': {
-    day: {
-      description: 'Light Snow',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png'
-    },
-    night: {
-      description: 'Light Snow',
-      image: 'http://openweathermap.org/img/wn/13n@2x.png'
-    }
-  },
-  '73': {
-    day: {
-      description: 'Snow',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png'
-    },
-    night: {
-      description: 'Snow',
-      image: 'http://openweathermap.org/img/wn/13n@2x.png'
-    }
-  },
-  '75': {
-    day: {
-      description: 'Heavy Snow',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png'
-    },
-    night: {
-      description: 'Heavy Snow',
-      image: 'http://openweathermap.org/img/wn/13n@2x.png'
-    }
-  },
-  '77': {
-    day: {
-      description: 'Snow Grains',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png'
-    },
-    night: {
-      description: 'Snow Grains',
-      image: 'http://openweathermap.org/img/wn/13n@2x.png'
-    }
-  },
-  '80': {
-    day: {
-      description: 'Light Showers',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Light Showers',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '81': {
-    day: {
-      description: 'Showers',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Showers',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '82': {
-    day: {
-      description: 'Heavy Showers',
-      image: 'http://openweathermap.org/img/wn/09d@2x.png'
-    },
-    night: {
-      description: 'Heavy Showers',
-      image: 'http://openweathermap.org/img/wn/09n@2x.png'
-    }
-  },
-  '85': {
-    day: {
-      description: 'Light Snow Showers',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png'
-    },
-    night: {
-      description: 'Light Snow Showers',
-      image: 'http://openweathermap.org/img/wn/13n@2x.png'
-    }
-  },
-  '86': {
-    day: {
-      description: 'Snow Showers',
-      image: 'http://openweathermap.org/img/wn/13d@2x.png'
-    },
-    night: {
-      description: 'Snow Showers',
-      image: 'http://openweathermap.org/img/wn/13n@2x.png'
-    }
-  },
-  '95': {
-    day: {
-      description: 'Thunderstorm',
-      image: 'http://openweathermap.org/img/wn/11d@2x.png'
-    },
-    night: {
-      description: 'Thunderstorm',
-      image: 'http://openweathermap.org/img/wn/11n@2x.png'
-    }
-  },
-  '96': {
-    day: {
-      description: 'Light Thunderstorms With Hail',
-      image: 'http://openweathermap.org/img/wn/11d@2x.png'
-    },
-    night: {
-      description: 'Light Thunderstorms With Hail',
-      image: 'http://openweathermap.org/img/wn/11n@2x.png'
-    }
-  },
-  '99': {
-    day: {
-      description: 'Thunderstorm With Hail',
-      image: 'http://openweathermap.org/img/wn/11d@2x.png'
-    },
-    night: {
-      description: 'Thunderstorm With Hail',
-      image: 'http://openweathermap.org/img/wn/11n@2x.png'
-    }
-  }
+type WeatherPeriod = {
+  description: string;
+  image: string;
 };
 
-export default defineComponent({
-  name: 'WeatherIcon',
-  data() {
-    return {
-      codeData
-    };
-  },
-  props: {
-    code: {
-      type: Number,
-      required: true
+type WeatherInfo = {
+  day: WeatherPeriod;
+  night: WeatherPeriod;
+};
+
+type CodeDataType = {
+  [key: string]: WeatherInfo;
+};
+
+const codeData: CodeDataType = {
+  "0": {
+    day: {
+      description: "Sunny",
+      image: "weather-clear",
     },
-    dayOrNight: {
-      type: String,
-      required: true
-    }
+    night: {
+      description: "Clear",
+      image: "weather-clear-night",
+    },
   },
-  computed: {
-    getIcon() {
-      // @ts-ignore
-      return this.codeData[(this.code.toString())][this.dayOrNight].image;
-    }
+  "1": {
+    day: {
+      description: "Mainly Sunny",
+      image: "weather-clear",
+    },
+    night: {
+      description: "Mainly Clear",
+      image: "weather-clear-night",
+    },
+  },
+  "2": {
+    day: {
+      description: "Partly Cloudy",
+      image: "weather-few-clouds",
+    },
+    night: {
+      description: "Partly Cloudy",
+      image: "weather-few-clouds-night",
+    },
+  },
+  "3": {
+    day: {
+      description: "Cloudy",
+      image: "weather-overcast",
+    },
+    night: {
+      description: "Cloudy",
+      image: "weather-overcast",
+    },
+  },
+  "45": {
+    day: {
+      description: "Foggy",
+      image: "weather-fog",
+    },
+    night: {
+      description: "Foggy",
+      image: "weather-fog",
+    },
+  },
+  "48": {
+    day: {
+      description: "Rime Fog",
+      image: "weather-fog",
+    },
+    night: {
+      description: "Rime Fog",
+      image: "weather-fog",
+    },
+  },
+  "51": {
+    day: {
+      description: "Light Drizzle",
+      image: "weather-showers-scattered",
+    },
+    night: {
+      description: "Light Drizzle",
+      image: "weather-showers-scattered",
+    },
+  },
+  "53": {
+    day: {
+      description: "Drizzle",
+      image: "weather-showers-scattered",
+    },
+    night: {
+      description: "Drizzle",
+      image: "weather-showers-scattered",
+    },
+  },
+  "55": {
+    day: {
+      description: "Heavy Drizzle",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Heavy Drizzle",
+      image: "weather-showers",
+    },
+  },
+  "56": {
+    day: {
+      description: "Light Freezing Drizzle",
+      image: "weather-showers-scattered",
+    },
+    night: {
+      description: "Light Freezing Drizzle",
+      image: "weather-showers-scattered",
+    },
+  },
+  "57": {
+    day: {
+      description: "Freezing Drizzle",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Freezing Drizzle",
+      image: "weather-showers",
+    },
+  },
+  "61": {
+    day: {
+      description: "Light Rain",
+      image: "weather-showers-scattered",
+    },
+    night: {
+      description: "Light Rain",
+      image: "weather-showers-scattered",
+    },
+  },
+  "63": {
+    day: {
+      description: "Rain",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Rain",
+      image: "weather-showers",
+    },
+  },
+  "65": {
+    day: {
+      description: "Heavy Rain",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Heavy Rain",
+      image: "weather-showers",
+    },
+  },
+  "66": {
+    day: {
+      description: "Light Freezing Rain",
+      image: "weather-showers-scattered",
+    },
+    night: {
+      description: "Light Freezing Rain",
+      image: "weather-showers-scattered",
+    },
+  },
+  "67": {
+    day: {
+      description: "Freezing Rain",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Freezing Rain",
+      image: "weather-showers",
+    },
+  },
+  "71": {
+    day: {
+      description: "Light Snow",
+      image: "weather-snow",
+    },
+    night: {
+      description: "Light Snow",
+      image: "weather-snow",
+    },
+  },
+  "73": {
+    day: {
+      description: "Snow",
+      image: "weather-snow",
+    },
+    night: {
+      description: "Snow",
+      image: "weather-snow",
+    },
+  },
+  "75": {
+    day: {
+      description: "Heavy Snow",
+      image: "weather-snow",
+    },
+    night: {
+      description: "Heavy Snow",
+      image: "weather-snow",
+    },
+  },
+  "77": {
+    day: {
+      description: "Snow Grains",
+      image: "weather-snow",
+    },
+    night: {
+      description: "Snow Grains",
+      image: "weather-snow",
+    },
+  },
+  "80": {
+    day: {
+      description: "Light Showers",
+      image: "weather-showers-scattered",
+    },
+    night: {
+      description: "Light Showers",
+      image: "weather-showers-scattered",
+    },
+  },
+  "81": {
+    day: {
+      description: "Showers",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Showers",
+      image: "weather-showers",
+    },
+  },
+  "82": {
+    day: {
+      description: "Heavy Showers",
+      image: "weather-showers",
+    },
+    night: {
+      description: "Heavy Showers",
+      image: "weather-showers",
+    },
+  },
+  "85": {
+    day: {
+      description: "Light Snow Showers",
+      image: "weather-snow",
+    },
+    night: {
+      description: "Light Snow Showers",
+      image: "weather-snow",
+    },
+  },
+  "86": {
+    day: {
+      description: "Snow Showers",
+      image: "weather-snow",
+    },
+    night: {
+      description: "Snow Showers",
+      image: "weather-snow",
+    },
+  },
+  "95": {
+    day: {
+      description: "Thunderstorm",
+      image: "weather-storm",
+    },
+    night: {
+      description: "Thunderstorm",
+      image: "weather-storm",
+    },
+  },
+  "96": {
+    day: {
+      description: "Light Thunderstorms With Hail",
+      image: "weather-storm",
+    },
+    night: {
+      description: "Light Thunderstorms With Hail",
+      image: "weather-storm",
+    },
+  },
+  "99": {
+    day: {
+      description: "Thunderstorm With Hail",
+      image: "weather-storm",
+    },
+    night: {
+      description: "Thunderstorm With Hail",
+      image: "weather-storm",
+    },
+  },
+};
+
+const iconPath: Ref<string> = ref("");
+const weatherInfo: Ref<WeatherInfo | null> = ref(null);
+const props = defineProps<{
+  code: number;
+  dayOrNight: "day" | "night";
+}>();
+
+onMounted(async () => {
+  weatherInfo.value = codeData[String(props.code)];
+  if (weatherInfo) {
+    iconPath.value = await getIconSource(weatherInfo.value[props.dayOrNight].image);
+  } else {
+    iconPath.value = "weather-severe-alert"; // Default icon for unknown codes
   }
 });
 </script>
 
 <template>
   <transition name="icon-fade" mode="out-in">
-    <img 
-      :key="getIcon" 
-      :src="getIcon" 
-      alt="weather icon" 
-      class="img-fluid weather-icon-img" 
+    <img
+      :src="iconPath"
+      :alt="weatherInfo ? weatherInfo[dayOrNight].description : 'Unknown weather condition'"
+      :title="weatherInfo ? weatherInfo[dayOrNight].description : 'Unknown weather condition'"
+      class="img-fluid weather-icon-img h-24"
     />
   </transition>
 </template>
