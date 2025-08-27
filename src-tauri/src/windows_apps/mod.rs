bc37f58c16e47cbecdb9a86e657f3ce5491707d8
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-27 08:07:29 -0300

    feat: agregar applet de Bluetooth y estilos correspondientes

--- src-tauri/src/windows_apps/mod.rs
+++ src-tauri/src/windows_apps/mod.rs
@@ -1,8 +1,10 @@
+pub mod applets;
 pub mod control_center;
 pub mod desktop;
 pub mod menu;
 pub mod panel;
 
+pub use applets::create_applet_bluetooth_window;
 pub use control_center::create_control_center_window;
 pub use desktop::create_desktops;
 pub use menu::create_menu_window;
