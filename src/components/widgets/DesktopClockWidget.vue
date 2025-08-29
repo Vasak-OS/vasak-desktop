95dca54859b278f1ef42a7ac9c52d089bccaabbf
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-29 12:07:16 -0300

    feat: actualizar estilos del componente DesktopClockWidget para usar la clase de fondo redondeado

--- src/components/widgets/DesktopClockWidget.vue
+++ src/components/widgets/DesktopClockWidget.vue
@@ -9,7 +9,7 @@ setInterval(() => {
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center h-auto p-4 rounded-vsk bg-gray-900/50 backdrop-blur-lg shadow-lg mb-4">
+    <div class="flex flex-col items-center justify-center h-auto p-4 rounded-vsk background backdrop-blur-lg shadow-lg mb-4">
         <h1 class="text-6xl font-bold text-white">{{ time }}</h1>
     </div>
 </template>
