227000f47db02cd6db18f171107e99b68519e9fe
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-09-26 16:48:35 -0300

    feat: agregar rutas y vistas para la configuración de la aplicación

---
+++ src/views/apps/configuration/ConfigNetworkView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import NetworkControlArea from '@/components/areas/network/NetworkControlArea.vue';
+import ConfigAppLayout from '@/layouts/ConfigAppLayout.vue';
+
+</script>
+
+<template>
+  <ConfigAppLayout>
+    <NetworkControlArea class="w-[calc(100%-96px)]" hide-x />
+  </ConfigAppLayout>
+</template>
