0fd8b82276b18985432dc6995bf65656c56ef596
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-12-18 15:44:29 -0300

    refactor: improve type safety and structure in App.vue and BluetoothDeviceCard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import { useConfigStore } from "@vasakgroup/plugin-config-manager";
+import { Store } from 'pinia';
 import { listen } from "@tauri-apps/api/event";
 import { onMounted, onUnmounted } from "vue";
 
 let unlistenConfig: Function | null = null;
 
 onMounted(async () => {
-  const configStore = useConfigStore();
-  configStore.loadConfig();
+  const configStore = useConfigStore() as Store<"config", { config: any; loadConfig: () => Promise<void>; }>;
+  await configStore.loadConfig();
   unlistenConfig = await listen("config-changed", async () => {
     document.startViewTransition(() => {
       configStore.loadConfig();
