81ce986dcae7c095eb1268655753f5ff6883fc23
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-01-02 12:09:54 -0300

    feat(vite): mejorar la configuración del servidor para permitir el acceso al sistema de archivos

--- vite.config.ts
+++ vite.config.ts
@@ -19,15 +19,18 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
+    fs: {
+      allow: ['..'],
+    },
     port: 1420,
     strictPort: true,
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
