3a5b138716b082168e4254b960abc444509aeeed
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-01-30 19:51:04 -0300

    feat: agregar logging en varios módulos para mejorar la trazabilidad de errores y eventos

--- src/views/applets/NetworkAppletView.vue
+++ src/views/applets/NetworkAppletView.vue
@@ -2,6 +2,7 @@
 import NetworkControlArea from '@/components/areas/network/NetworkControlArea.vue';
 import { invoke } from '@tauri-apps/api/core';
 import { onMounted } from 'vue';
+import { logError } from '@/utils/logger';
 
 onMounted(async () => {
 	document.addEventListener('keydown', (event) => {
@@ -9,7 +10,7 @@ onMounted(async () => {
 			try {
 				invoke('toggle_network_applet');
 			} catch (error) {
-				console.error('Error al cerrar:', error);
+				logError('Error al cerrar applet de red:', error);
 			}
 		}
 	});
