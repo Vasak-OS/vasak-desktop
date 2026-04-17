4bd0899dd9e25e7ef89a0a9683dcea3e36df5728
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-04-17 14:33:23 -0300

    refactor: eliminar el módulo de información del sistema y sus referencias

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -15,7 +15,6 @@ mod search_window;
 mod session;
 mod shortcuts;
 mod system_config;
-mod system_info;
 mod tray;
 mod window_manager;
 
@@ -42,7 +41,6 @@ pub use search_window::toggle_search;
 pub use session::{detect_display_server, logout, reboot, shutdown, suspend};
 pub use shortcuts::{add_custom_shortcut, check_shortcut_conflicts, delete_shortcut, execute_shortcut, get_shortcuts, update_shortcut, ShortcutsState};
 pub use system_config::{get_system_config, set_system_config, get_current_system_state, get_gtk_themes, get_cursor_themes, get_icon_packs};
-pub use system_info::{get_system_info, get_cpu_usage_only, get_memory_usage_only};
 pub use tray::{get_tray_items, init_sni_watcher};
 pub use window_manager::{get_windows, toggle_window};
 
