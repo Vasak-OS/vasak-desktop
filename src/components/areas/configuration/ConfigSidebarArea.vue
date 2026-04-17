8ee88be83625adf7a6b93b2aa6c0a9d977fe2b1b
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-04-17 19:07:35 -0300

    refactor: eliminar la vista de configuración de estilos y su botón asociado

--- src/components/areas/configuration/ConfigSidebarArea.vue
+++ src/components/areas/configuration/ConfigSidebarArea.vue
@@ -6,7 +6,6 @@ import ConfigSidebarButton from '@/components/buttons/ConfigSidebarButton.vue';
 <template>
   <div class="w-20 bg-ui-bg/80 h-full rounded-corner flex flex-col items-center p-2 gap-2">
     <ul>
-      <ConfigSidebarButton icon="style" to="/apps/configuration/style" title="Style & Theme Settings" />
       <ConfigSidebarButton icon="keyboard" to="/apps/configuration/shortcuts" title="Keyboard Shortcuts" />
       <ConfigSidebarButton icon="network" to="/apps/configuration/network" title="Network Settings" />
     </ul>
